aa8718c99959fe578997868e941142e434d9f4da
Thiago Menezes <thimenezes19@gmail.com>
2023-09-19 18:25:18 -0300

    JavaScript quase pronto
    paginação concluida porém ainda não está responsivo e as imagens ainda estão com erro

--- script.js
+++ script.js
@@ -1,37 +1,124 @@
 
-function mostrarResultado(resultado) {
+// fetch('http://servicodados.ibge.gov.br/api/v3/noticias/?tipo=noticia/?qtd=3')
+//   .then(res => {
+//     return res.json();
+//   })
+//   .then(data => {
+//     const dados = data;
+//     const containerLeft = document.getElementById('noticia')
+//     for (const item of dados.items){
+//       const h1 = document.createElement("h1")
+//       const img = document.createElement("img")
+//       const span = document.createElement("span")
+//       const link = document.createElement("a")
+//       const p = document.createElement("p")
+//       h1.textContent = item.titulo
+//       img.src = item.imagens.url
+//       span.textContent = item.data_publicacao
+//       p.textContent = item.introducao
+//       link.href = item.link
+//       link.textContent = "Ler Mais"
+//       link.target = "_blank"
 
-    console.log(resultado);
-}
-fetch('http://servicodados.ibge.gov.br/api/v3/noticias/?tipo=noticia/?qtd=3')
-  .then(res => {
-    return res.json();
-  })
-  .then(data => {
-    const dados = data;
-    const containerLeft = document.getElementById('noticia')
-    for (const item of dados.items){
-      const h1 = document.createElement("h1")
-      const img = document.createElement("img")
-      const span = document.createElement("span")
-      const link = document.createElement("a")
+//       containerLeft.appendChild(h1)
+//       containerLeft.appendChild(img)
+//       containerLeft.appendChild(span)
+//       containerLeft.appendChild(p)
+//       containerLeft.appendChild(link)
+      
+//     }
+//   })
+//   .catch(error => {
+//     console.error('Ocorreu um erro:', error);
+//   });
+let currentPage = 1;
+const itemsPerPage = 1;
+async function cards(pagina) {
+    const response = await fetch(`http://servicodados.ibge.gov.br/api/v3/noticias/?tipo=noticia&qtd=${itemsPerPage + 3}&page=${pagina}`);
+    if (!response.ok) {
+      throw new Error('Erro na solicitação da API');
+    }
+    const data = await response.json();
+    const noticias = data.items;
+
+    const cards = document.getElementById('cards');
+    cards.innerHTML = '';
+    
+    const h2 = document.createElement("h2")
+    cards.appendChild(h2);
+    h2.textContent = "Outras Notícias"
+
+    for (const noticia of noticias) {
       const p = document.createElement("p")
-      h1.textContent = item.titulo
-      img.src = item.imagens.url
-      span.textContent = item.data_publicacao
-      p.textContent = item.introducao
-      link.href = item.link
+      const a = document.createElement("a")
+
+      p.textContent = noticia.titulo
+      a.textContent = "Ver notícia"
+      a.href = noticia.link
+      a.target = "_blank"
+
+      cards.appendChild(p);
+      cards.appendChild(a);
+    }}
+
+async function carregarPagina(pagina) {
+  try {
+    const response = await fetch(`http://servicodados.ibge.gov.br/api/v3/noticias/?tipo=noticia&qtd=${itemsPerPage}&page=${pagina}`);
+    if (!response.ok) {
+      throw new Error('Erro na solicitação da API');
+    }
+    const data = await response.json();
+    const noticias = data.items;
+
+    const containerLeft = document.getElementById('noticia');
+    containerLeft.innerHTML = '';
+
+    for (const noticia of noticias) {
+      const h1 = document.createElement("h1");
+      const img = document.createElement("img");
+      const span = document.createElement("span");
+      const p = document.createElement("p");
+      const link = document.createElement("a")
+
+      h1.textContent = noticia.titulo
+      img.src = noticia.imagens[0].url
+      span.textContent = noticia.data_publicacao
+      p.textContent = noticia.introducao
+      link.href = noticia.link
       link.textContent = "Ler Mais"
       link.target = "_blank"
 
-      containerLeft.appendChild(h1)
-      containerLeft.appendChild(img)
-      containerLeft.appendChild(span)
-      containerLeft.appendChild(p)
+      containerLeft.appendChild(h1);
+      containerLeft.appendChild(img);
+      containerLeft.appendChild(span);
+      containerLeft.appendChild(p);
       containerLeft.appendChild(link)
-      
     }
-  })
-  .catch(error => {
+
+    const nextButton = document.getElementById("next-button");
+    const prevButton = document.getElementById("prev-button");
+
+    nextButton.disabled = noticias.length < itemsPerPage;
+    prevButton.disabled = currentPage === 1;
+  } catch (error) {
     console.error('Ocorreu um erro:', error);
-  });
+  }
+}
+document.getElementById("next-button").addEventListener("click", () => {
+  currentPage++;
+  carregarPagina(currentPage);
+  cards(currentPage);
+});
+
+document.getElementById("prev-button").addEventListener("click", () => {
+  if (currentPage > 1) {
+    currentPage--;
+    carregarPagina(currentPage);
+    cards(currentPage);
+  }
+});
+
+carregarPagina(currentPage);
+cards(currentPage);
+
+
