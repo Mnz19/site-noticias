78f5272e8782d7e231a78cbaf81f7fa1e16a8a31
Thiago Menezes <thimenezes19@gmail.com>
2023-09-19 19:54:12 -0300

    Bug das imagens concertado
    Para concertar era necessario transformar a imagem para json para poder pegar seu link

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let currentPage = 1;
-const itemsPerPage = 1;
+let currentPage = 1
+const itemsPerPage = 1
 async function cards(pagina) {
   try{
-    const response = await fetch(`https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=noticia&qtd=${itemsPerPage + 3}&page=${pagina}`);
+    const response = await fetch(`https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=noticia&qtd=${itemsPerPage + 3}&page=${pagina}`)
     if (!response.ok) {
-      throw new Error('Erro na solicitação da API');
+      throw new Error('Erro na solicitação da API')
     }
-    const data = await response.json();
-    const noticias = data.items;
+    const data = await response.json()
+    const noticias = data.items
 
-    const cards = document.getElementById('cards');
-    cards.innerHTML = '';
+    const cards = document.getElementById('cards')
+    cards.innerHTML = ''
 
     const h2 = document.createElement("h2")
-    cards.appendChild(h2);
+    cards.appendChild(h2)
     h2.textContent = "Outras Notícias"
 
     for (const noticia of noticias) {
@@ -25,68 +25,69 @@ async function cards(pagina) {
       a.href = noticia.link
       a.target = "_blank"
 
-      cards.appendChild(p);
-      cards.appendChild(a);
+      cards.appendChild(p)
+      cards.appendChild(a)
     }}catch (error) {
-      console.error('Ocorreu um erro:', error);
+      console.error('Ocorreu um erro:', error)
     }}
 
 async function carregarPagina(pagina) {
   try {
-    const response = await fetch(`https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=noticia&qtd=${itemsPerPage}&page=${pagina}`);
+    const response = await fetch(`https://servicodados.ibge.gov.br/api/v3/noticias/?tipo=noticia&qtd=${itemsPerPage}&page=${pagina}`)
     if (!response.ok) {
-      throw new Error('Erro na solicitação da API');
+      throw new Error('Erro na solicitação da API')
     }
-    const data = await response.json();
-    const noticias = data.items;
+    const data = await response.json()
+    const noticias = data.items
 
-    const containerLeft = document.getElementById('noticia');
-    containerLeft.innerHTML = '';
+    const containerLeft = document.getElementById('noticia')
+    containerLeft.innerHTML = ''
 
     for (const noticia of noticias) {
-      const h1 = document.createElement("h1");
-      const img = document.createElement("img");
-      const span = document.createElement("span");
-      const p = document.createElement("p");
+      const h1 = document.createElement("h1")
+      const imagemNoticia = document.createElement("img")
+      const span = document.createElement("span")
+      const p = document.createElement("p")
       const link = document.createElement("a")
 
+      let jsonImagem = JSON.parse(noticia.imagens)
+      imagemNoticia.src = 'https://agenciadenoticias.ibge.gov.br/' + jsonImagem.image_intro
       h1.textContent = noticia.titulo
-      img.src = noticia.imagens.url
       span.textContent = noticia.data_publicacao
       p.textContent = noticia.introducao
       link.href = noticia.link
       link.textContent = "Ler Mais ..."
       link.target = "_blank"
 
-      containerLeft.appendChild(h1);
-      containerLeft.appendChild(img);
-      containerLeft.appendChild(span);
-      containerLeft.appendChild(p);
+      containerLeft.appendChild(h1)
+      containerLeft.appendChild(imagemNoticia)
+      containerLeft.appendChild(span)
+      containerLeft.appendChild(p)
       containerLeft.appendChild(link)
     }
 
-    const nextButton = document.getElementById("next-button");
-    const prevButton = document.getElementById("prev-button");
+    const nextButton = document.getElementById("next-button")
+    const prevButton = document.getElementById("prev-button")
 
-    nextButton.disabled = noticias.length < itemsPerPage;
-    prevButton.disabled = currentPage === 1;
+    nextButton.disabled = noticias.length < itemsPerPage
+    prevButton.disabled = currentPage === 1
   } catch (error) {
-    console.error('Ocorreu um erro:', error);
+    console.error('Ocorreu um erro:', error)
   }
 }
 document.getElementById("next-button").addEventListener("click", () => {
-  currentPage++;
-  carregarPagina(currentPage);
-  cards(currentPage);
-});
+  currentPage++
+  carregarPagina(currentPage)
+  cards(currentPage)
+})
 
 document.getElementById("prev-button").addEventListener("click", () => {
   if (currentPage > 1) {
-    currentPage--;
-    carregarPagina(currentPage);
-    cards(currentPage);
+    currentPage--
+    carregarPagina(currentPage)
+    cards(currentPage)
   }
-});
+})
 
-carregarPagina(currentPage);
-cards(currentPage);
+carregarPagina(currentPage)
+cards(currentPage)
